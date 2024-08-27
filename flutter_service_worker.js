bf4ec6c2936b596d549e9e8676b71d5016c54a9a
陳子傑(波西米亞●陳傑) <114125629+bsbacon0966@users.noreply.github.com>
2024-08-27 18:20:31 +0800

    大調整，讓firebase的RW不要這麼大

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "bd6629738c5e2ca2fa732aa749e019d1",
+"flutter_bootstrap.js": "b85ae533e8ca0c33a2da1e91df3f4ede",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "857827bde47f83425082e78f25c6eb5b",
 "/": "857827bde47f83425082e78f25c6eb5b",
-"main.dart.js": "94a6c31a20bb8ec7b4c3098cad28a1d7",
+"main.dart.js": "2e383bb38a558ad5f90e9ec559a5a9e7",
 "manifest.json": "b7da0d5addf02fda40eab669fa1f408f",
 "version.json": "01680542c0ffbd48fc0934b11fb1a224"};
 // The application shell files that are downloaded before a service worker can
