c631623bd74a47fcc5924e0dd13a5555a5f6991a
陳子傑(波西米亞●陳傑) <114125629+bsbacon0966@users.noreply.github.com>
2024-09-17 21:34:00 +0800

    double line chart is avaliable

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2478424330"
+    serviceWorkerVersion: "564437060"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "5bc187dcc230f17dff19c29e863bbc84",
+"flutter_bootstrap.js": "395626bdc44004663e8952e1427b64d1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "69e06928c9836cdcf9305b836aff759e",
 "/": "69e06928c9836cdcf9305b836aff759e",
-"main.dart.js": "969807bad6e46e29bd55d56b1786f84c",
+"main.dart.js": "06282802b1a2017dab7e34df4a6d8c8c",
 "manifest.json": "b7da0d5addf02fda40eab669fa1f408f",
 "version.json": "01680542c0ffbd48fc0934b11fb1a224"};
 // The application shell files that are downloaded before a service worker can
